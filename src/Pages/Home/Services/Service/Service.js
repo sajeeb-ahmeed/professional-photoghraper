99ca8271323b86ae029035a61f2c09254b1946d9
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-04-18 05:55:11 +0600

    done

--- src/Pages/Home/Services/Service/Service.js
+++ src/Pages/Home/Services/Service/Service.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 const Service = ({ singleService }) => {
